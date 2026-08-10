5d844663b6c7d9f8439e7449b3ebc089a8c90919
OM TIWARI <121350344+iamomtiwari@users.noreply.github.com>
2026-08-10 18:30:03 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.py
@@ -0,0 +1,9 @@
+#Brute force plan:
+#sorting
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s)!=len(t):return False
+
+        if sorted(s)==sorted(t):
+                return True
+        return False
